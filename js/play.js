f75ae03094d72b6a1cfd10c06b6a12a77d82ff7f
root <root@cvm-3iqxkwe2da225.novalocal>
2023-12-20 02:49:28 +0800

    add game

--- js/play.js
+++ js/play.js
@@ -32,20 +32,19 @@ if (window.top != window) {
 		cocoMessage.warning("正在配置资源！", 2000);
 		const showload = document.getElementById('btn_load');
 		//展示游戏名称
-		let gnm = gameInfo.v;
-		if (gnm != 'false') {
-			gnm = '(' + gnm + ')';
-		} else {
-			gnm = '';
-		}
+		const gnm = !gameInfo.v ? '' : '(' + gameInfo.v + ')';
 		document.getElementById('name').innerHTML = gameInfo.n + gnm;
 		// 修改title
-		document.title = gameInfo.n + gnm + ' - ' + 'GBA游戏盒';
+		document.title = gameInfo.n + gnm + ' - ' + '街机游戏盒';
 		window.gameName = gameInfo.n + gnm;
 		// 游戏ID
 		window.gameId = gameInfo.i;
 		// 封面
 		window.backgroundImg = '../imgs/' + gameInfo.i + '.png';
+		//BIOS
+		if (gameInfo.b) {
+			window.biosUrl = '../bios/' + gameInfo.b + '.7z';
+		}
 		// ROM
 		window.gameUrl = "https://arcade-rom.heheda.top/" + gameInfo.i + ".zip";
 		// 初始化
